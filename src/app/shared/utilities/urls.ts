cee37b092059c05ab7b9f974a2243ae4a0ecaac1
Zankrut97 <65472192+Zankrut97@users.noreply.github.com>
2021-05-09 23:47:45 -0300

    Update urls.ts

--- src/app/shared/utilities/urls.ts
+++ src/app/shared/utilities/urls.ts
@@ -1,5 +1,5 @@
 class Url{
-    static readonly PERSON_API_ENDPOINT:String = "/redspace/api/people/";
+    static readonly PERSON_API_ENDPOINT:String = "https://node-starwars-backend.herokuapp.com/redspace/api/people/";
 }
 
 export default Url;
